f3189f0cd03e4787c0641cad1c8bc3c59a837033
pentol <123047914+ohioscript@users.noreply.github.com>
2025-06-14 09:53:01 +0800

    Add files via upload

--- dnslegasi/setup-dns.sh
+++ dnslegasi/setup-dns.sh
@@ -18,7 +18,7 @@ GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 CYAN='\e[36m'
 LIGHT='\033[0;37m'
-host="www.vpnlegasi.com"
+host="https://raw.githubusercontent.com"
 directory="public/netflixchecker"
 MYIP=$(wget -qO- ipinfo.io/ip)
 echo "Checking VPS"
@@ -47,17 +47,18 @@ echo -e "\033[0;34m------------------------------------\033[0m"
 mkdir -p /opt/dnslegasi
 mkdir -p dnslegasi
 cd dnslegasi
-curl "http://${host}/vpnlegasi/admin/dnslegasi/sewa-proxy-domains.txt" >> domains
-wget "http://${host}/vpnlegasi/admin/dnslegasi/dnslegasi"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/dnsmasq.sh"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/sniproxy.sh"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/my_init"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/services.ini"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/Dockerfile"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/instl"
-wget -O /usr/bin/speedtest "http://${host}/vpnlegasi/admin/dnslegasi/speedtest_cli.py" && chmod +x /usr/bin/speedtest
-wget -O /usr/bin/xp-dns "http://${host}/vpnlegasi/admin/dnslegasi/sewa-xp-dns.sh" && chmod +x /usr/bin/xp-dns
-wget -O /usr/bin/menu http://${host}/vpnlegasi/admin/dnslegasi/sewa-menu.sh && chmod +x /usr/bin/menu
+rm -rf domains  > /dev/null 2>&1
+curl "http://${host}/ohioscript/main/dnslegasi/sewa-proxy-domains.txt" >> domains
+wget "http://${host}/ohioscript/main/dnslegasi/dnslegasi"
+wget "http://${host}/ohioscript/main/dnslegasi/dnsmasq.sh"
+wget "http://${host}/ohioscript/main/dnslegasi/sniproxy.sh"
+wget "http://${host}/ohioscript/main/dnslegasi/my_init"
+wget "http://${host}/ohioscript/main/dnslegasi/services.ini"
+wget "http://${host}/ohioscript/main/dnslegasi/Dockerfile"
+wget "http://${host}/ohioscript/main/dnslegasi/instl"
+wget -O /usr/bin/speedtest "http://${host}/ohioscript/main/dnslegasi/speedtest_cli.py" && chmod +x /usr/bin/speedtest
+wget -O /usr/bin/xp-dns "http://${host}/ohioscript/main/dnslegasi/sewa-xp-dns.sh" && chmod +x /usr/bin/xp-dns
+wget -O /usr/bin/menu http://${host}/ohioscript/main/dnslegasi/sewa-menu.sh && chmod +x /usr/bin/menu
 apt install jq -y && wget -O /usr/bin/menu_nf http://${host}/public/netflixchecker/menu_nf.sh && chmod +x /usr/bin/menu_nf
 apt install snapd -y
 snap install fast
@@ -107,17 +108,18 @@ echo -e "\033[0;34m------------------------------------\033[0m"
 mkdir -p /opt/dnslegasi
 mkdir -p dnslegasi
 cd dnslegasi
-curl "http://${host}/vpnlegasi/admin/dnslegasi/main-proxy-domains.txt" >> domains
-wget "http://${host}/vpnlegasi/admin/dnslegasi/dnslegasi"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/dnsmasq.sh"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/sniproxy.sh"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/my_init"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/services.ini"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/Dockerfile"
-wget "http://${host}/vpnlegasi/admin/dnslegasi/instl"
-wget -O /usr/bin/speedtest "http://${host}/vpnlegasi/admin/dnslegasi/speedtest_cli.py" && chmod +x /usr/bin/speedtest
-wget -O /usr/bin/xp-dns "http://${host}/vpnlegasi/admin/dnslegasi/main-xp-dns.sh" && chmod +x /usr/bin/xp-dns
-wget -O /usr/bin/menu http://${host}/vpnlegasi/admin/dnslegasi/main-menu.sh && chmod +x /usr/bin/menu
+rm -rf domains  > /dev/null 2>&1
+curl "http://${host}/ohioscript/main/dnslegasi/main-proxy-domains.txt" >> domains
+wget "http://${host}/ohioscript/main/dnslegasi/dnslegasi"
+wget "http://${host}/ohioscript/main/dnslegasi/dnsmasq.sh"
+wget "http://${host}/ohioscript/main/dnslegasi/sniproxy.sh"
+wget "http://${host}/ohioscript/main/dnslegasi/my_init"
+wget "http://${host}/ohioscript/main/dnslegasi/services.ini"
+wget "http://${host}/ohioscript/main/dnslegasi/Dockerfile"
+wget "http://${host}/ohioscript/main/dnslegasi/instl"
+wget -O /usr/bin/speedtest "http://${host}/ohioscript/main/dnslegasi/speedtest_cli.py" && chmod +x /usr/bin/speedtest
+wget -O /usr/bin/xp-dns "http://${host}/ohioscript/main/dnslegasi/main-xp-dns.sh" && chmod +x /usr/bin/xp-dns
+wget -O /usr/bin/menu http://${host}/ohioscript/main/dnslegasi/main-menu.sh && chmod +x /usr/bin/menu
 apt install jq -y && wget -O /usr/bin/menu_nf http://${host}/public/netflixchecker/menu_nf.sh && chmod +x /usr/bin/menu_nf
 apt install snapd -y
 snap install fast
@@ -126,7 +128,7 @@ docker build -t vpnlegasi/dnslegasi .
 docker container run -d -p 53:53 -p 80:80 -p 443:443 vpnlegasi/dnslegasi:latest
 ln -snf $PWD/dnslegasi /usr/local/bin/dnslegasi
 chmod +x /usr/local/bin/dnslegasi
-rm -rf resolver.conf
+rm -rf resolver.conf  > /dev/null 2>&1
 echo 127.0.0.1 >> resolver.conf
 dnslegasi start
 sleep 5
@@ -167,7 +169,7 @@ echo -e "\033[0;34m------------------------------------\033[0m"
 echo -e "\E[44;1;39m      Complete Install DNS Server   \E[0m"
 echo -e "\033[0;34m------------------------------------\033[0m"
 timedatectl set-timezone Asia/Kuala_Lumpur
-rm -rf *.sh
+rm -rf *.sh  > /dev/null 2>&1
 echo -e "Server Will Reboot in 10sec"
 sleep 10
 reboot
